d82044d204fe58e3274df11dc2f08e733467d4f0
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-04-16 19:06:26 -0600

    Add test to validate presence of name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,8 @@ describe Merchant, test: :model do
   describe 'attributes' do
     it { should have_db_column(:name).of_type(:string) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
 end
