18d607a932e5dc1a3ba04fdc6e58a4f76b35ae4e
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-04-16 19:13:12 -0600

    Create merchants_request_index_spec.rb

---
+++ spec/requests/api/v1/merchants_request_index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+context Api::V1::Merchants, type: :request do
+  describe 'Fetch all merchants' do
+    it 'Can fetch the first 20 merchants by default' do
+
+    end
+  end
+end
