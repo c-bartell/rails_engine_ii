5a80564f9b1b4faa8f599c9d92d44446566eb4e7
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-04-20 19:13:23 -0600

    Add Merchant index route and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Merchant resources
+  get 'api/v1/merchants', to: 'api/v1/merchants#index'
 end
